2e4dddd8fd5a9ce366c3a56d8ff73cf07861ccdd
Shaik Kashif <shaikkashif73@gmail.com>
2026-04-01 15:27:11 +0530

    making the cosmos rotate

--- src/components/Page1Bottom.jsx
+++ src/components/Page1Bottom.jsx
@@ -1,8 +1,18 @@
 import React from "react";
 import goldenCosmos from "../assets/goldenCosmos.gif";
 import redCosmos from "../assets/redCosmos.gif";
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 
 const Page1Bottom = () => {
+  useGSAP(function () {
+    gsap.to("#banner img", {
+      rotate: 360,
+      duration: 5,
+      repeat: -1,
+      ease: "linear",
+    });
+  });
   return (
     <div className="absolute left-0 p-13 flex items-end justify-between bottom-0 w-full ">
       <div>
@@ -11,8 +21,8 @@ const Page1Bottom = () => {
         </h2>
         <h3 className="text-1xl text-gray-400"> BESPOKE FREELANCE</h3>
       </div>
-      <div>
-        <img className="  h-20 w-20 rounded-full" src={redCosmos} alt="" />
+      <div id="banner">
+        {/* <img className="  h-20 w-20 rounded-full" src={redCosmos} alt="" /> */}
         <img className=" h-20 w-20 rounded-full" src={goldenCosmos} alt="" />
       </div>
     </div>
